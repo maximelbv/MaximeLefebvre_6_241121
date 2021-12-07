95909e409e78fb0f5ee83f02752abcea6ccbd445
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2021-12-07 13:54:20 +0100

    create signup & login logics

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,22 +1,46 @@
 import user from '../models/auth.js';
+import bcrypt from 'bcrypt';
 
+// 'export function signupPost()' ou 'exports.signupPost' ?
 export function signupPost(req, res) {
-    // delete req.body._id; Pourquoi?
-    // verifier si un compte avec cet email existe déjà?
+    // 'delete req.body._id' dans le tuto, pourquoi?
+
+    // cryptage du mot de passe, il prend en paramètres la donnée à crypter et le nombre d'executions de l'algorithme de hashage (SALT)
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+
         const newUser = new user({
-            // hash du password
-            ...req.body
-        })        
+            email: req.body.email,
+            password: hash
+        })
+
         return newUser.save()
-            .then(() => res.status(201).json({message: 'Le compte a bien été créé'}))
-            .catch(() => res.status(400).json({error: 'erreure'}))
-        ;
-        // renvoyer vers la page de connexion dans le '.then?'
+        .then(() => res.status(201).json({message: 'Le compte a bien été créé'}))
+        .catch(error => res.status(400).json({ error }))
+
+    })
+    .catch(error => res.status(500).json({ error }));
 };
 
 export function loginPost(req, res){
-    console.log(req);
+
     user.findOne({email: req.body.email})
-    .then(newUser => res.status(200).json(newUser))
-    .catch(error => res.status(404).json({error: 'error'}))
+    .then(user => {
+        if (!user) {
+            return res.status(401).json({ error: 'Utilisateur non trouvé' })
+        } 
+        bcrypt.compare(req.body.password, user.password)
+
+        .then(valid => {
+            if (!valid) {
+                return res.status(401).json({ error: 'Mot de passe incorrect' })
+            }
+            res.status(200).json({
+                userId: user._id
+            })
+        })
+        .catch(error => res.status(500).json({ error }))
+    })
+    .catch(error => res.status(500).json({ error }))
+
 }
